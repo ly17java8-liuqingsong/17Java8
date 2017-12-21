27f2923ad730ee86e2ea356fb4d481de2d6ae575
何治通 <hetoney@gmail.com>
2017-12-21 15:40:02 +0800

    更新目录

--- 04 SQLServer2005基础/第6章 课程设计/上机作业/邢聪  项目实训/添加表.sql
+++
@@ -1,33 +0,0 @@
---商品类别表
-create table Category(
---自动增长，从1开始，主键
-    Cid int primary key identity(1,1),--类型名称，不能为空且不能重复    cname nvarchar(10) not null unique,--类别描述，可以为空    Description nvarchar(20)
-) 
---商品信息表
-create table Ware(
---商品的编号（即条形码），主键
-   Wid int primary key identity(1,1),--商品名称，必填且不能重复   Wname nvarchar(20) not null unique,
---商品所属的类别（外键），必填
-   Cid int not null foreign key references Category(Cid),
---商品进价，必须大于0
-   PurchasePrice nvarchar(30)  check(len(PurchasePrice)>0),--商品售价，必须大于0   SalesPrice nvarchar(30) check(len(SalesPrice)>0),--商品库存量，必须大于0   Amount nvarchar(30) check(len(Amount)>0)
-)
---员工表
-create table Employee(
---员工编号，主键，自动增长，从1001开始
-    Eid int primary key identity(1001,1),--员工姓名，必填    Ename nvarchar(3) not null,--员工密码，不少于六位    Epassword varchar(10) check(len(Epassword)>6),--备注，可不填    Remark nvarchar(30) 
-)  
---销售记录表
-create table SalesInfo(
---主键，自动增长，从1开始
-    Sid int primary key identity(1,1),
---所销售的商品的编号（外键），必填
-    Wid  int not null foreign key references ware(Wid),
---商品销售的时间，必填
-    SalesDate nvarchar(20) not null,--商品销售的数量，必填且必须大于0    SalesAmount nvarchar(30) not null check(len(SalesAmount)>0),--收银员的编号（外键），必填    Eid int not null foreign key references Employee(Eid)
-)
---删除表                                                                                                                                                                                                                 
-drop table Employee
-drop table Ware
-drop table Category
-drop table SalesInfo
