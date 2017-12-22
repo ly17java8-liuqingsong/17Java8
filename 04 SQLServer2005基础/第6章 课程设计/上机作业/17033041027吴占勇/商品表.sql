a4400135ec2eb846f635d5bc883cc2342f7209ba
LY17JAVA-wuzhanyong <34700821+LY17JAVA-wuzhanyong@users.noreply.github.com>
2017-12-22 17:26:28 +0800

    Add files via upload

---
+++ 04 SQLServer2005基础/第6章 课程设计/上机作业/17033041027吴占勇/商品表.sql
@@ -0,0 +1,21 @@
+--用户表
+create table person(
+
+pid int primary key identity(1,1),
+
+pname nvarchar(10) not null check(len(pname)>1),
+
+Epassword nvarchar(18) unique,
+
+)
+
+
+--号码表
+create table tel(
+
+tid int primary key identity(1,1),
+
+tname nvarchar(11)  unique,
+
+state int not null,)--已购号码表create table Stel(sid int primary key identity(1,1),stid int not null foreign key references tel(tid),scode int not null foreign key references person(pid),--注册时间Atime datetime,--注销时间Btime datetime,--最后充值时间ctime datetime,--余额money decimal(10,2)default 0)--删除表drop table persondrop table teldrop table Stel
+
